1dba97b1cb2c4f5bc36e94851c57562fb4fbae0d
Bogdan Polak <bogdanpol@gmail.com>
2021-03-24 22:25:05 +0100

    Printed weather

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import credentials, src.services.forecast, lists
 lists.run()
 
 src.services.forecast.rapidApiSecureKey = credentials.rapidapi_key
-# cityCode = "san francisco,us"
-# cityCode = "atlanta,us"
-# cityCode = "denver,us"
-# cityCode = "edinburgh,uk"
-cityCode = "mumbai,in"
-print(src.services.forecast.getForecast(cityCode))
 
+cityCodes = ["atlanta,us", "denver,us", "edinburgh,uk", "warsaw,pl", "mumbai,in"]
+
+for cityCode in cityCodes:
+    forecast = src.services.forecast.getForecast(cityCode)
+    entry = forecast["list"][0]
+    dateTime = entry["dt_txt"]
+    temperature = round(entry["main"]["temp"]-273.15,2)
+    fealsLike = round(entry["main"]["feels_like"]-273.15,2)
+    windSpeed = str(round(entry["wind"]["speed"],2))+"m/s"
+    clouds = str(entry["clouds"]["all"])+"%"
+    weatherDesc = entry["weather"][0]["description"]
+    clouds = str(entry["clouds"]["all"])+"%"
+    print(cityCode, dateTime, temperature, fealsLike, windSpeed, clouds, weatherDesc, sep="\t")
+
+# print(src.services.forecast.getForecast("atlanta,us")["list"][0])
